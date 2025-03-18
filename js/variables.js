b1f1c57f97f8d3ae302592a0445944e724cc00f2
Fr4nkFletcher <135678866+Fr4nkFletcher@users.noreply.github.com>
2025-03-17 21:05:38 -0500

    3.2 statusbar fixes

--- js/variables.js
+++ js/variables.js
@@ -62,13 +62,13 @@ const MCYD32NOGPSlatestFiles = {
 const MCYD32CAPGPSlatestFiles = {
     'bootloader': 'resources/STATIC/M/CYD/esp32_marauder.ino.bootloader.bin',
     'partitions': 'resources/STATIC/M/CYD/esp32_marauder.ino.partitions.bin',
-    'firmware': 'resources/CURRENT/esp32_marauder_v1_2_1_20250314_cyd32CAP_gps.bin',
+    'firmware': 'resources/CURRENT/esp32_marauder_v1_2_1_20250317_cyd32CAP_gps.bin',
 };
 // Latest Marauder for CYD32CAPNOGPS
 const MCYD32CAPNOGPSlatestFiles = {
     'bootloader': 'resources/STATIC/M/CYD/esp32_marauder.ino.bootloader.bin',
     'partitions': 'resources/STATIC/M/CYD/esp32_marauder.ino.partitions.bin',
-    'firmware': 'resources/CURRENT/esp32_marauder_v1_2_1_20250314_cyd32CAP_nogps.bin',
+    'firmware': 'resources/CURRENT/esp32_marauder_v1_2_1_20250317_cyd32CAP_nogps.bin',
 };
 // Latest Marauder for CYD35GPS
 const MCYD35GPSlatestFiles = {
